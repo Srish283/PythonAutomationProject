fbaed5908f4a8b2ae8bfa07c2b8390d017ed4a86
SionaNazareth <71539472+SionaNazareth@users.noreply.github.com>
2020-12-02 11:41:02 +0530

    Update jsontopdf.py

--- Project/jsontopdf.py
+++ Project/jsontopdf.py
@@ -1,8 +1,21 @@
+#import fpdf module
 from fpdf import FPDF
+
+# save FPDF() class into  a variable pdf
 pdf = FPDF()
+
+# Add a page 
 pdf.add_page()
+   
+# set style and size of font that you want in the pdf 
 pdf.set_font("Arial", size = 5.3)
+
+# open the text file in read mode 
 f = open("file.json", "r")
+
+# insert the texts in pdf
 for x in f:
     pdf.cell(100, 6, txt = x, ln = 1, align = 'A')
-pdf.output("mygfg.pdf")
+    
+# save the pdf with name .pdf 
+pdf.output("MyProject.pdf")
