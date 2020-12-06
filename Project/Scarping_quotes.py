16ef4cab39f5d7cc8fd4dd44e3d7263486efb8d9
sudhipra <74848916+sudhipra@users.noreply.github.com>
2020-12-06 12:42:31 +0530

    Update Scarping_quotes.py

--- Project/Scarping_quotes.py
+++ Project/Scarping_quotes.py
@@ -1,31 +1,38 @@
 #-----Webscraping  and storing the information in the .csv file--------
 
+
 import requests
 from bs4 import BeautifulSoup
 import csv
 import json
-from fpdf import FPDF 
+from fpdf import FPDF
+print('Mini Project to Demonstrate Working of following Concepts:  ')
+print("---------------------------------------------------------------------")
 
+print('1.Webscraping the data')
+print('2.Storing the scraped data in the .csv file')
+print('3.Converting the .csv file to .json file')
+print('4.Converting the .json file to pdf')
 
+print("---------------------------------------------------------------------")
 def open_url():
 
     #Enter the url from where u want to search the quotes
-    #URL = "http://www.values.com/inspirational-quotes" <----- Use this url
-
-    URL = input('Enter a url: ')
+    #URL = "http://www.values.com/inspirational-quotes"
+    print("Lets webscrap the data from the URL and store it in .csv file")
+    URL = "http://www.values.com/inspirational-quotes"
     r = requests.get(URL)
+    print('--- URL is entered by the user ---')
 
     try:
-        r.raise_for_status()
-        
+        r.raise_for_status() #checks for error
     except Exception as exc:
         print('There was a problem: %s' % (exc))
-
     soup = BeautifulSoup(r.content, 'html5lib')
-   
+
     quotes=[]  # a list to store quotes
 
-    table = soup.find('div', attrs = {'id':'all_quotes'})
+    table = soup.find('div', attrs = {'id':'all_quotes'}) # finds attributes where id is all_quotes
 
     for row in table.findAll('div',
                             attrs = {'class':'col-6 col-lg-3 text-center margin-30px-bottom sm-margin-30px-top'}):
@@ -35,65 +42,75 @@ def open_url():
         quote['img'] = row.img['src']
         quotes.append(quote)
 
-    filename = 'inspirational_quote.csv'
-    with open(filename, 'w', newline='') as f:
+    filename = 'inspirational_quote.csv' # filename is inspirational_quote.csv
+    with open(filename, 'w', newline='') as f:  # opens the file in write mode
         w = csv.DictWriter(f,['theme','url','img'])
-        w.writeheader()
-        
+        w.writeheader() # writes the header (theme,url,img)
+
         for quote in quotes:
-            w.writerow(quote)
+            w.writerow(quote) # writes the quotes
+    print('Operation sucessfull')
+
+    print('---Quotes successfully stored in csv file---')
 
 
 #Converting .csv file  to .json file
 
 def csv_to_json():
-    csvfile = open('inspirational_quote.csv', 'r')
+    print("---------------------------------------------------------------------")
+    print("Lets convert the csv file to json file")
+    csvfile = open('inspirational_quote.csv', 'r') # opens the file in read mode
+    print('-----csv file is opened!-----')
 
-    jsonfile = open('file.json', 'w')
+    jsonfile = open('file.json', 'w') # opens the file in write mode
+    print('-----New json file is created-----')
 
     fieldnames = ('theme','url','img')
 
-    reader = csv.DictReader( csvfile, fieldnames)
+    reader = csv.DictReader( csvfile, fieldnames) # read file using DictReader
+    print('-----All the fields in the csv file are stored in json Successfully-----')
+
     for row in reader:
-        json.dump(row, jsonfile)
-        jsonfile.write('\n')
+        json.dump(row, jsonfile) #converts the Python objects into appropriate json objects
+        jsonfile.write('\n')    #writes
+
+    print('-----Successfully converted csv file to json file-----')
 
 
 #Converting .json file to .pdf file
 
 
 def json_to_pdf():
+    print("---------------------------------------------------------------------")
+    print("Lets convert the json file to pdf file")
 
-    # save FPDF() class into  a variable pdf 
-    pdf = FPDF()    
-    
-    # Add a page into the pdf created
-    pdf.add_page() 
-    
-    # set style and size of font that you want in the pdf 
-    pdf.set_font("Arial", size = 5.3) 
-    
-    # open the .json file in read mode 
-    f = open("file.json", "r") 
-    
-    # insert the texts in pdf created
-    for x in f: 
-        pdf.cell(0, 10, txt= x, ln = 2, align='A') 
-        
-        
-    # save the pdf with name .pdf 
-    pdf.output("MyProject.pdf")   
+    pdf = FPDF() # save FPDF() class into a variable pdf
+    print('-----pdf is being created!-----')
+    pdf.add_page() # Add a page
 
-#Function Calls   
-if __name__=="__main__":
-    #search for the quotes and store in .csv file
+    print('-----A Page is created in the pdf!-----')
+
+    pdf.set_font("Arial", size = 5.3) # set style and size of font that you want in the pdf
+
+    f = open("file.json", "r") # open the text file in read mode
+
+    for x in f:
+        pdf.cell(0, 10, txt= x, ln = 2, align='A') # insert the texts in pdf
+        #print(x)
+
+    pdf.output("MyProject.pdf") # save the pdf with name .pdf
+
+    print('-----Converted a json file to pdf successfully-----')
+
+    print("---------------------------------------------------------------------")
+
+if __name__=="__main__": #Function Calls
     open_url()
 
-    #Converts inspirational_quote.csv file to file.json file
     csv_to_json()
 
-    #Converts file.json to MyProject.pdf
     json_to_pdf()
+
 
     
 
